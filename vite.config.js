106d5568e3b4cc9db2574b78cb5cb758554207e6
DippaFuddDippaFuddgit config --global user.email hawkinstroy@aol.com <hawkinstroy@aol.com>
2025-06-03 12:44:19 -0400

    updated files

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'], // Add any other problematic modules here
-    },
-  },
+  base: './', // Adjust if needed for Vercel
 });
